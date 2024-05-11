2e458589423b82817f52703f0dadbfa7e0a74e4a
hiroyasu33 <assassinhiro0330@gmail.com>
2024-05-11 22:56:00 +0900

    ご確認お願いします。

---
+++ src/animal/Human.java
@@ -0,0 +1,32 @@
+package animal;
+
+public class Human extends Animal implements Thinkable {
+    // フィールド
+    private String hobby;
+
+    // コンストラクタ
+    public Human() {
+    }
+
+    // 引数ありコンストラクタ
+    public Human(String name, int age, String hobby) {
+        super.setName(name);
+        super.setAge(age);
+        this.hobby = hobby;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+    @Override
+    public void think() {
+        System.out.println("私は" + hobby + "について考えています。");
+
+
+    }
+
+}
